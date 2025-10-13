2e162fffca2bdee8a76c53a6c56c9fac4c7863ff
Vishwasdabhi <152868196+Vishwasdabhi@users.noreply.github.com>
2025-10-13 11:55:17 +0530

    Update Data_path.cpp

--- Data_path.cpp
+++ Data_path.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int GPR[32];
+int DM[1024 * 1024];
 class Controller{
     public:
     bool regRead;
@@ -79,6 +81,16 @@ Controller :: Controller(string opcode){
         branch = 0;
         Jump = 1;
         ALUOp = "11";
+    }else if(opcode == "1100111"){
+        regRead = 1;
+        regWrite = 1;
+        ALUSrc = 0;
+        memRead = 0;
+        memWrite = 0;
+        mem2reg = 0;
+        branch = 0;
+        Jump = 1;
+        ALUOp = "11";
     }
 }
 
@@ -88,15 +100,31 @@ string ALUControl(string ALUOp, string func7, string func3){
         return "0010";
     }
     if(ALUOp == "01"){
-        return "0110";
+        if(func3 == "000")
+            return "0110";
+        if(func3 == "100")
+            return "0011";
+        if(func3 == "101")
+            return "0111";
     }
     if(ALUOp == "10"){
-        if(func7[6] == '1'){
+        if(func7[1] == '1'){
             if(func3 == "000"){
                 return "0110";
             }
         }
+        else if(func7[6] == '1'){
+            if(func3 == "000"){
+                return "0100";
+            }
+            if(func3 == "110"){
+                return "0101";
+            }
+        }
         else{
+            if(func3 == "001"){
+                return 1000;
+            }
             if(func3 == "000"){
                 return "0010";
             }
@@ -111,7 +139,7 @@ string ALUControl(string ALUOp, string func7, string func3){
     return "0000";
 }
 
-string signedExtend(string s){
+int signedExtend(string s){
     string ans = s;
     reverse(ans.begin(),ans.end());
     char ch = ans.back();
@@ -119,12 +147,141 @@ string signedExtend(string s){
         ans += ch;
     }
     reverse(ans.begin(),ans.end());
+    int ans_num = 0;
+    int sign = (ans[0] == '1') ? -1 : 1;
+    for(int i = 1;i < 32;i++){
+        ans_num = ans_num * 2 + (ans[i] - '0');
+    }
+    return ans_num * sign;
+}
+
+int getDecimalU(string s){
+    int num = 0;
+    reverse(s.begin(),s.end());
+    for(int i = 0;i < s.size();i++){
+        if(s[i] == '1'){
+            num += (1 << i);
+        }
+    }
+    return num;
+}
+int getimm(string ins){
+    string opcode = ins.substr(26);
+    if(opcode == "" || opcode ==  "" || opcode == ""){
+        return signedExtend(ins.substr(0,12));
+    }
+    if(opcode == ""){
+        string str = ""
+        str += ins[0];
+        str += ins.substr(12,8);
+        str += ins[11];
+        str += ins.substr(1,10);
+        return signedExtend(str);
+    }
+    if(opcode == "" || opcode == ""){
+        return signedExtend(ins.substr(0,7) + ins.substr(20,5));
+    }
+}
+int getGPR(string s){
+    int num = 0;
+    reverse(s.begin(),s.end());
+    for(int i = 0;i < s.size();i++){
+        if(s[i] == '1'){
+            num += (1 << i);
+        }
+    }
+    return GPR[num];
+}
+void setGPR(string s,int val){
+    int num = 0;
+    reverse(s.begin(),s.end());
+    for(int i = 0;i < s.size();i++){
+        if(s[i] == '1'){
+            num += (1 << i);
+        }
+    }
+    GPR[num] = val;
+}
+
+int ALU(string ALUSelect,int alusrc1,int alusrc2){
+    if(ALUSelect == "0000"){
+        return (alusrc1 & alusrc2);
+    }
+    if(ALUSelect == "0001"){
+        return (alusrc1 | alusrc2);
+    }
+    if(ALUSelect == "0010"){
+        return (alusrc1 + alusrc2);
+    }
+    if(ALUSelect == "0011"){
+        return (alusrc1 < alusrc2) ? 0 : 1;
+    }
+    if(ALUSelect == "0100"){
+        return alusrc1 * alusrc2;
+    }
+    if(ALUSelect == "0101"){
+        return alusrc1 % alursrc2;
+    }
+    if(ALUSelect == "0110"){
+        return alusrc1 - alusrc2;
+    }
+    if(ALUSelect == "0111"){
+        return (alusrc1 >= alusrc2) ? 0 : 1;
+    }
+    if(ALUSelect == "1000"){
+        if(alusrc2 > 30)return 0;
+        return (alusrc1 << alusrc2);
+    }
     return ans;
 }
 
-
+int outputselect(int mem2reg, int jump,int ALUResult,int LDResult, int NPC){
+    if(mem2reg)return LDResult;
+    if(jump)return NPC;
+    return ALUResult;
+}
 int main(){
     freopen("output.txt","r",stdin);
-                                                                                                                                                                                                            
+    vector<string> ins_v;
+    string str;
+    while(getline(cin,str)){
+        ins_v.push_back(str);
+    }
+    int sz = ins_v.size();
+    int PC = 0;
+    while(PC < sz){
+        string ins = ins_v[PC];
+        int NPC = PC + 1;
+        int JPC = NPC + (signedExtend(ins.substr(0,20)) / 4);
+        string rsl1 = ins.substr(12,5);
+        string rsl2 = ins.substr(7,5);
+        string rdl = ins.substr(20,5);
+        string opcode = ins.substr(26);
+        string func3 = ins.substr(17,3);
+        string func7 = ins.substr(0,7);
+        int imm = getimm(ins);
+        Controller *CW = new Controller(opcode);
+        int rs1 = 0;
+        if(CW->regRead) rs1 = get_GPR(rsl1);
+        int rs2 = 0;
+        if(CW->regRead) rs2 = get_GPR(rsl2);
+        int alrsrc2 = 0;
+        if(CW->ALUSrc) alusrc2 = imm;
+        else alusrc2 = rs2;
+        int alusrc1 = rs1;
+        CW->ALUSelect = ALUControl(CW->ALUOp,func7,func3);
+        int ALUResult = ALU(CW->ALUSelect, alusrc1,alusrc2);
+        int ALUZeroFlag = (ALUResult == 0);
+        int BPC = ((imm / 4)<< 1) + PC;
+        int TPC = 0;
+        if(CW->branch && ALUZeroFlag) TPC = BPC;
+        else TPC = NPC;
+        if(CW->Jump) TPC = JPC;
+        int LDResult  = 0;
+        if(CW->memRead) LDResult = DM[ALUResult];
+        if(CW->memWrite) DM[ALUResult] = rs2;
+        if(CW->regWrite) setGPR(rdl, outputselect(CW->mem2reg,CW->Jump, ALUResult, LDResult, NPC));
+        PC = TPC;
+    }
     return 0;
 }
