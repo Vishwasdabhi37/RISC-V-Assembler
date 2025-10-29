351e1d5d378b0d67a49ff5cd4705288bccbeeaaf
Vishwasdabhi37 <152868443+Vishwasdabhi37@users.noreply.github.com>
2025-10-29 23:25:01 +0530

    Refactor Controller class and improve methods

--- Data_path.cpp
+++ Data_path.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 int GPR[32];
 int DM[1024 * 1024];
-class Controller{
-    public:
+class Controller
+{
+public:
     bool regRead;
     bool regWrite;
     bool ALUSrc;
@@ -17,11 +18,26 @@ class Controller{
     string ALUSelect;
     Controller();
     Controller(string opcode);
+    void print();
 };
-Controller :: Controller(){}
-Controller :: Controller(string opcode){
+void Controller ::print()
+{
+    cout << regRead << ' ' << regWrite << ' ' << ALUSrc << ' ' << memRead << ' ' << memWrite << ' ' << mem2reg << ' ' << branch
+         << ' ' << Jump << ' ' << ALUOp << ' ' << ALUSelect << endl;
+}
+Controller ::Controller()
+{
+    regRead = regWrite = ALUSrc = memRead = memWrite = mem2reg = branch = Jump = 0;
+    ALUOp = "00";
     ALUSelect = "0000";
-    if(opcode == "0110011"){
+}
+Controller ::Controller(string opcode)
+{
+    regRead = regWrite = ALUSrc = memRead = memWrite = mem2reg = branch = Jump = 0;
+    ALUOp = "00";
+    ALUSelect = "0000";
+    if (opcode == "0110011")
+    {
         regRead = 1;
         regWrite = 1;
         ALUSrc = 0;
@@ -31,7 +47,9 @@ Controller :: Controller(string opcode){
         branch = 0;
         Jump = 0;
         ALUOp = "10";
-    }else if(opcode == "0010011"){
+    }
+    else if (opcode == "0010011")
+    {
         regRead = 1;
         regWrite = 1;
         ALUSrc = 1;
@@ -41,7 +59,9 @@ Controller :: Controller(string opcode){
         branch = 0;
         Jump = 0;
         ALUOp = "10";
-    }else if(opcode == "0000011"){
+    }
+    else if (opcode == "0000011")
+    {
         regRead = 1;
         regWrite = 1;
         ALUSrc = 1;
@@ -51,7 +71,9 @@ Controller :: Controller(string opcode){
         branch = 0;
         Jump = 0;
         ALUOp = "00";
-    }else if(opcode == "0100011"){
+    }
+    else if (opcode == "0100011")
+    {
         regRead = 1;
         regWrite = 0;
         ALUSrc = 1;
@@ -61,7 +83,9 @@ Controller :: Controller(string opcode){
         branch = 0;
         Jump = 0;
         ALUOp = "00";
-    }else if(opcode == "1100011"){
+    }
+    else if (opcode == "1100011")
+    {
         regRead = 1;
         regWrite = 0;
         ALUSrc = 0;
@@ -71,9 +95,11 @@ Controller :: Controller(string opcode){
         branch = 1;
         Jump = 0;
         ALUOp = "01";
-    }else if(opcode == "1101111"){
+    }
+    else if (opcode == "1101111")
+    {
         regRead = 0;
-        regWrite = 1;
+        regWrite = 0;
         ALUSrc = 0;
         memRead = 0;
         memWrite = 0;
@@ -81,10 +107,12 @@ Controller :: Controller(string opcode){
         branch = 0;
         Jump = 1;
         ALUOp = "11";
-    }else if(opcode == "1100111"){
+    }
+    else if (opcode == "1100111")
+    {
         regRead = 1;
         regWrite = 1;
-        ALUSrc = 0;
+        ALUSrc = 1;
         memRead = 0;
         memWrite = 0;
         mem2reg = 0;
@@ -94,44 +122,58 @@ Controller :: Controller(string opcode){
     }
 }
 
-//change the alu operation from string to pointer to the controller
-string ALUControl(string ALUOp, string func7, string func3){
-    if(ALUOp == "00"){
+// change the alu operation from string to pointer to the controller
+string ALUControl(string ALUOp, string func7, string func3)
+{
+    if (ALUOp == "00")
+    {
         return "0010";
     }
-    if(ALUOp == "01"){
-        if(func3 == "000")
+    if (ALUOp == "01")
+    {
+        if (func3 == "000")
             return "0110";
-        if(func3 == "100")
+        if (func3 == "100")
             return "0011";
-        if(func3 == "101")
+        if (func3 == "101")
             return "0111";
     }
-    if(ALUOp == "10"){
-        if(func7[1] == '1'){
-            if(func3 == "000"){
+    if (ALUOp == "10")
+    {
+        if (func7[1] == '1')
+        {
+            if (func3 == "000")
+            {
                 return "0110";
             }
         }
-        else if(func7[6] == '1'){
-            if(func3 == "000"){
+        else if (func7[6] == '1')
+        {
+            if (func3 == "000")
+            {
                 return "0100";
             }
-            if(func3 == "110"){
+            if (func3 == "110")
+            {
                 return "0101";
             }
         }
-        else{
-            if(func3 == "001"){
+        else
+        {
+            if (func3 == "001")
+            {
                 return "1000";
             }
-            if(func3 == "000"){
+            if (func3 == "000")
+            {
                 return "0010";
             }
-            if(func3 == "111"){
+            if (func3 == "111")
+            {
                 return "0000";
             }
-            if(func3 == "110"){
+            if (func3 == "110")
+            {
                 return "0001";
             }
         }
@@ -139,163 +181,269 @@ string ALUControl(string ALUOp, string func7, string func3){
     return "0000";
 }
 
-int signedExtend(string s){
-    string ans = s;
-    reverse(ans.begin(),ans.end());
-    char ch = ans.back();
-    while((int)ans.size()!=32){
-        ans += ch;
+int signedExtend(string s)
+{
+    // string ans = s;
+    // reverse(ans.begin(),ans.end());
+    // char ch = ans.back();
+    // while((int)ans.size()!=32){
+    //     ans += ch;
+    // }
+    // reverse(ans.begin(),ans.end());
+    // int ans_num = 0;
+    // int sign = 1;
+    // if(ans[0] == '1'){
+    //     sign = -1;
+    //     bool flag = false;
+    //     for(int i = 32;i > 0;i--){
+    //         if(flag){
+    //             if(ans[i] == '1')
+    //                 ans[i] = '0';
+    //             else ans[i] = '1';
+    //         }
+    //         if(ans[i] == '1'){
+    //             flag = true;
+    //         }
+    //     }
+    // }
+    // for(int i = 1;i < 32;i++){
+    //     ans_num = ans_num * 2 + (ans[i] - '0');
+    // }
+    // return ans_num * sign;
+
+    if (s.empty())
+        return 0;
+    int n = s.size();
+    int val = 0;
+    for (int i = 0; i < n; i++)
+    {
+        val = (val << 1) | (s[i] - '0');
     }
-    reverse(ans.begin(),ans.end());
-    int ans_num = 0;
-    int sign = (ans[0] == '1') ? -1 : 1;
-    for(int i = 1;i < 32;i++){
-        ans_num = ans_num * 2 + (ans[i] - '0');
+    if (s[0] == '1' && n < 32)
+    {
+        val |= (~0u << n);
     }
-    return ans_num * sign;
+    return val;
 }
 
-int getDecimalU(string s){
+int getDecimalU(string s)
+{
     int num = 0;
-    reverse(s.begin(),s.end());
-    for(int i = 0;i < s.size();i++){
-        if(s[i] == '1'){
+    reverse(s.begin(), s.end());
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '1')
+        {
             num += (1 << i);
         }
     }
     return num;
 }
-int getimm(string ins){
-    string opcode = ins.substr(26);
-    if(opcode == "" || opcode ==  "" || opcode == ""){
-        return signedExtend(ins.substr(0,12));
+int getimm(string ins)
+{
+    string opcode = ins.substr(25);
+    if (opcode == "0010011" || opcode == "1100111" || opcode == "0000011")
+    {
+        return signedExtend(ins.substr(0, 12));
     }
-    if(opcode == ""){
+    if (opcode == "1101111")
+    {
         string str = "";
         str += ins[0];
-        str += ins.substr(12,8);
+        str += ins.substr(12, 8);
         str += ins[11];
-        str += ins.substr(1,10);
+        str += ins.substr(1, 10);
         return signedExtend(str);
     }
-    if(opcode == "" || opcode == ""){
-        return signedExtend(ins.substr(0,7) + ins.substr(20,5));
+    if (opcode == "1100011")
+    {
+        string str = "";
+        str += ins[0];
+        str += ins[20];
+        str += ins.substr(1, 6);
+        str += ins.substr(17, 4);
+        return signedExtend(str);
     }
+    if (opcode == "0100011")
+    {
+        // string str = ins.substr(0,7) + ins.substr(20,5);
+        // cout << str << endl;
+        return signedExtend(ins.substr(0, 7) + ins.substr(20, 5));
+    }
+
     return 0;
 }
-int get_GPR(string s){
+int get_GPR(string s)
+{
     int num = 0;
-    reverse(s.begin(),s.end());
-    for(int i = 0;i  < s.size();i++){
-        if(s[i] == '1'){
+    reverse(s.begin(), s.end());
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '1')
+        {
             num += (1 << i);
         }
     }
     return GPR[num];
 }
-void setGPR(string s,int val){
+void setGPR(string s, int val)
+{
     int num = 0;
-    reverse(s.begin(),s.end());
-    for(int i = 0;i < s.size();i++){
-        if(s[i] == '1'){
+    reverse(s.begin(), s.end());
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '1')
+        {
             num += (1 << i);
         }
     }
+    // cout << "reg " << num << ' ' << val << endl;
     GPR[num] = val;
 }
 
-int ALU(string ALUSelect,int alusrc1,int alusrc2){
-    if(ALUSelect == "0000"){
+int ALU(string ALUSelect, int alusrc1, int alusrc2)
+{
+    if (ALUSelect == "0000")
+    {
         return (alusrc1 & alusrc2);
     }
-    if(ALUSelect == "0001"){
+    if (ALUSelect == "0001")
+    {
         return (alusrc1 | alusrc2);
     }
-    if(ALUSelect == "0010"){
+    if (ALUSelect == "0010")
+    {
         return (alusrc1 + alusrc2);
     }
-    if(ALUSelect == "0011"){
+    if (ALUSelect == "0011")
+    {
         return (alusrc1 < alusrc2) ? 0 : 1;
     }
-    if(ALUSelect == "0100"){
+    if (ALUSelect == "0100")
+    {
         return alusrc1 * alusrc2;
     }
-    if(ALUSelect == "0101"){
+    if (ALUSelect == "0101")
+    {
         return alusrc1 % alusrc2;
     }
-    if(ALUSelect == "0110"){
+    if (ALUSelect == "0110")
+    {
         return alusrc1 - alusrc2;
     }
-    if(ALUSelect == "0111"){
+    if (ALUSelect == "0111")
+    {
         return (alusrc1 >= alusrc2) ? 0 : 1;
     }
-    if(ALUSelect == "1000"){
-        if(alusrc2 > 30)return 0;
+    if (ALUSelect == "1000")
+    {
+        if (alusrc2 > 30)
+            return 0;
         return (alusrc1 << alusrc2);
     }
     return 0;
 }
 
-int outputselect(int mem2reg, int jump,int ALUResult,int LDResult, int NPC){
-    if(mem2reg)return LDResult;
-    if(jump)return NPC;
+int outputselect(int mem2reg, int jump, int ALUResult, int LDResult, int NPC)
+{
+    if (mem2reg)
+        return LDResult;
+    if (jump)
+        return NPC;
     return ALUResult;
 }
-int main(){
-    freopen("Output_file.txt","r",stdin);
-    for(int i = 0;i < 32;i++){
-        GPR[i] = i;
+int main()
+{
+    // freopen("output.txt","r",stdin);
+    freopen("input_single_cycle.txt", "r", stdin);
+    for (int i = 0; i < 32; i++)
+    {
+        GPR[i] = 0;
     }
-    for(auto && it : GPR){
-            cout << it << ' ';
-        }
-        cout << endl;
+    int base_add = 4096;
+    GPR[8] = base_add;
+    int N = 15;
+    DM[base_add] = N;
+    for (int i = 1; i <= N; i++)
+    {
+        DM[base_add + i] = i;
+    }
     vector<string> ins_v;
     string str;
-    while(getline(cin,str)){
+    while (getline(cin, str))
         ins_v.push_back(str);
-    }
     int sz = ins_v.size();
     int PC = 0;
     int cnt = 0;
-    while(PC < sz){
+    while (PC < sz)
+    {
+        // cout << "PC " << PC << endl;
         string ins = ins_v[PC];
         int NPC = PC + 1;
-        int JPC = NPC + (signedExtend(ins.substr(0,20)) / 4);
-        string rsl1 = ins.substr(12,5);
-        string rsl2 = ins.substr(7,5);
-        string rdl = ins.substr(20,5);
-        string opcode = ins.substr(26);
-        string func3 = ins.substr(17,3);
-        string func7 = ins.substr(0,7);
+        string rsl1 = ins.substr(12, 5);
+        string rsl2 = ins.substr(7, 5);
+        string rdl = ins.substr(20, 5);
+        string opcode = ins.substr(25);
+        string func3 = ins.substr(17, 3);
+        string func7 = ins.substr(0, 7);
         int imm = getimm(ins);
-        Controller *CW = new Controller(opcode);
+        int JPC = NPC + imm;
+        // cout << "HERE" << endl;
+        // cout << opcode << endl;
+        // cout << imm << endl;
+        Controller CW(opcode);
+        // CW->print();
         int rs1 = 0;
-        if(CW->regRead) rs1 = get_GPR(rsl1);
+        if (CW.regRead)
+            rs1 = get_GPR(rsl1);
         int rs2 = 0;
-        if(CW->regRead) rs2 = get_GPR(rsl2);
+        if (CW.regRead)
+            rs2 = get_GPR(rsl2);
         int alusrc2 = 0;
-        if(CW->ALUSrc) alusrc2 = imm;
-        else alusrc2 = rs2;
+        if (CW.ALUSrc)
+            alusrc2 = imm;
+        else
+            alusrc2 = rs2;
         int alusrc1 = rs1;
-        CW->ALUSelect = ALUControl(CW->ALUOp,func7,func3);
-        int ALUResult = ALU(CW->ALUSelect, alusrc1,alusrc2);
+        // cout << alusrc1 << ' ' << alusrc2 << endl;
+        CW.ALUSelect = ALUControl(CW.ALUOp, func7, func3);
+        // cout << CW->ALUSelect << endl;
+        int ALUResult = ALU(CW.ALUSelect, alusrc1, alusrc2);
+        // cout << "ALU " << ALUResult << endl;
         int ALUZeroFlag = (ALUResult == 0);
-        int BPC = ((imm / 4)<< 1) + PC;
+        int BPC = imm + PC;
         int TPC = 0;
-        if(CW->branch && ALUZeroFlag) TPC = BPC;
-        else TPC = NPC;
-        if(CW->Jump) TPC = JPC;
-        int LDResult  = 0;
-        if(CW->memRead) LDResult = DM[ALUResult];
-        if(CW->memWrite) DM[ALUResult] = rs2;
-        if(CW->regWrite) setGPR(rdl, outputselect(CW->mem2reg,CW->Jump, ALUResult, LDResult, NPC));
-        PC = TPC;
-        cout << "INS : " << cnt++ << ' ';
-        for(auto && it : GPR){
-            cout << it << ' ';
+        if (CW.branch && ALUZeroFlag)
+            TPC = BPC;
+        else
+            TPC = NPC;
+        if (CW.Jump)
+            TPC = JPC;
+        int LDResult = 0;
+        // cout << ALUResult << endl;
+        if (CW.memRead)
+        {
+            // cout << "READ :" << ALUResult << ' ' << DM[ALUResult] << endl;
+            LDResult = DM[ALUResult];
         }
-        cout << endl;
+        if (CW.memWrite)
+        {
+            // cout << "STORE :" << ALUResult << '-' << rs2 << endl;
+            DM[ALUResult] = rs2;
+        }
+        if (CW.regWrite)
+        {
+            int tmp = outputselect(CW.mem2reg, CW.Jump, ALUResult, LDResult, NPC);
+            setGPR(rdl, tmp);
+        }
+        PC = TPC;
+        // cout << "INS : " << cnt++ << ':';
+        // for(auto && it : GPR){
+        //     cout << it << ' ';
+        // }
+        // cout << endl;
     }
+    // cout << "ANS :";
+    cout << GPR[4] << endl;
     return 0;
 }
